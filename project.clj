405a8aeab5966e08ac25594f28d372ee655c8500
Justin <justin2004@hotmail.com>
2021-06-06 12:02:42 -0400

    proj

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject hl7 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/java.classpath "1.0.0"]]
   :resource-paths ["built-for-hl7-stuff-shared-things-1.0.0-jar-with-dependencies.jar" "."]
